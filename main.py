40bdb90d1367698b5c32c1c68bfc5d5ec0fe554b
Mayank Patel <mayankpatel1715@gmail.com>
2025-11-29 19:26:48 +0530

    MVP 1.2

--- main.py
+++ main.py
@@ -13,15 +13,29 @@ def load_data():
 def save_data(bank_account):
     with open("bank_db.json", 'w') as file:
         json.dump(bank_account, file, indent=4)
-        
-def oneaccount(name,bank_account):
-    for account in bank_account:
-        if account["name"] == name:
-            account_display(account)
-            return
+
+def one_ID(id,bank_account):
+    for account in  bank_account:
+        if account["account_no"] == id:
+            return account
     else:
-        raise Exception("   Account not Found!  ")
-    
+        raise TypeError("Account not found!")
+
+def account_ID_valid():
+    while True:
+        acc_id = input("Enter your account ID: ")
+        try:
+            id = int(acc_id)
+        except ValueError:
+            print("Account ID must be an integer")
+            continue
+        
+        if id < 0:
+            print("ID canot be Negative")
+            continue
+        return acc_id
+            
+
 def account_display(info):
     print("----"*15)
     print("\n")
@@ -48,17 +62,12 @@ def account_creation(bank_account):
     
     bank_account.append(form)
     
-    oneaccount(name,bank_account)
+    account_display(form)
     save_data(bank_account)
-    
+
 def account_info(bank_account):
-    acc_id = int(input("Enter your account ID: "))
-    
-    for account in  bank_account:
-        if account["account_no"] == acc_id:
-            return account
-    else:
-        raise Exception("   Account not found!    ")
+    acc_id = account_ID_valid()  
+    return one_ID(acc_id,bank_account)
 
 
 def deposit_money(bank_account):
@@ -73,7 +82,7 @@ def deposit_money(bank_account):
             return
         
     print("   Account not found!  ")
-    
+
 def withdraw_money(bank_account):
     acc_id = int(input("Enter your account ID : "))
     money = int(input("Enter the amount of money you want to withdraw : ₹ "))
@@ -97,7 +106,7 @@ def delete_account(bank_account):
             return 
             
     print("Account not found!")
-    
+
 def main():
     
     bank_account = load_data()
@@ -113,27 +122,30 @@ def main():
         print("6. Exit App ")
         
         choice = input("Enter your Choice : ")
-        # print(bank_account)
-        # print(type(bank_account))
+
+        try: 
+            match choice:
+                case '1':
+                    account_creation(bank_account)
+                case '2':
+                    info = account_info(bank_account)
+                    account_display(info)
+                case '3':
+                    deposit_money(bank_account)
+                case '4':
+                    withdraw_money(bank_account)
+                case '5':
+                    delete_account(bank_account)
+                case '6':
+                    break
+                case _:
+                    print("Invalid Choice.")
         
-        # print(bank_account["account_no"])
-        
-        match choice:
-            case '1':
-                account_creation(bank_account)
-            case '2':
-                info = account_info(bank_account)
-                account_display(info)
-            case '3':
-                deposit_money(bank_account)
-            case '4':
-                withdraw_money(bank_account)
-            case '5':
-                delete_account(bank_account)
-            case '6':
-                break
-            case _:
-                print("Invalid Choice.")
+        except TypeError as e:
+            print(e)
+            
+        except ValueError as e:
+            print(e)
                 
 if __name__ == "__main__":
     main()
